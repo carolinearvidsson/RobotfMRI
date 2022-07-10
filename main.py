28513a2b2151147d309760ff585bcf5dd9c95927
Caroline <caroline.arvidsson@ling.su.se>
2022-07-10 17:12:17 +0200

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from onset_and_durations import OnsetsDurations
 
 if __name__== '__main__':
-    mdic = OnsetsDurations(sys.argv[1]).final_output
+    mdic = OnsetsDurations(sys.argv[1]).final_output #check what is saved
     for run in mdic:
         filename = 'onset_and_durations_ignore_ovrl' + run[0:3] + '-' + run[5:7] + '_' + 'run' + run[-1] + '.mat'
         savemat(filename, mdic[run])
