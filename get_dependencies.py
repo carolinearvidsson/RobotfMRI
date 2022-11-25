4af49cfd3e5bf670d94f61b00cf6a3bbd21daa59
Caroline <caroline.arvidsson@ling.su.se>
2022-11-25 09:42:49 +0100

    Update get_dependencies.py

--- get_dependencies.py
+++ get_dependencies.py
@@ -3,15 +3,14 @@ import textdescriptives as td
 
 nlp = spacy.load("fr_dep_news_trf")
 nlp.add_pipe('textdescriptives')
-doc = nlp("c'est intéressant")
 for token in doc:
     print(token._.dependency_distance)
 spacy.displacy.serve(doc, style="dep")
 
-doc = nlp("c'est une campagne de pub ciblant les enfants")
+doc = nlp("ouais c'est vrai que le fait d'utiliser des super héros euh")
 spacy.displacy.serve(doc, style="dep")
 
-doc = nlp("ça fait sens avec ce qu'on a vu précédemment")
+doc = nlp("ouais c'est pour faire le le thème super-héros")
 spacy.displacy.serve(doc, style="dep")
 
 doc = nlp("je sais pas elle semblait contrariée cette framboise")
